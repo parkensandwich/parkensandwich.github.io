147553d3e7ae405fd11a7da43fe7b6f94b4ccf19
Orhan Toy <toyorhan@gmail.com>
2017-09-19 00:49:35 +0200

    Refactor use of Google Maps JS
    1. Use latest JS API
    2. Use API key
    3. Bring in unminified JS and use callback to initiate the map
    4. Remove Google Maps <script> tags from pages where it is not used

--- assets/pages/find-d29d455a27d455742f28d4b41f40373b.js
+++ assets/pages/find-d29d455a27d455742f28d4b41f40373b.js
@@ -1,1 +1,32 @@
-$(function(){var e={center:new google.maps.LatLng(55.216056,11.756894),scrollwheel:!1,zoom:17,mapTypeId:google.maps.MapTypeId.ROADMAP,styles:[{featureType:"poi",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"road",elementType:"labels",stylers:[{visibility:"on"}]}]},a=new google.maps.Map(document.getElementById("map-canvas"),e),o=new google.maps.Marker({position:new google.maps.LatLng(55.216056,11.756894)});o.setMap(a)});
+window.initMapOnFindPage = function() {
+  var myOptions = {
+    center: new google.maps.LatLng(55.216056, 11.756894),
+    scrollwheel: false,
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [
+      {
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "labels",
+        stylers: [
+          { visibility: "on" },
+        ]
+      },
+    ],
+  };
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(55.216056, 11.756894),
+  });
+
+  marker.setMap(map);
+}
